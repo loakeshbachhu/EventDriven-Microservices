f19e5eb1e38e352f1d0ca68ea01865706e61ba64
Loakesh Bachhu <bachhu.loakesh@gmail.com>
2021-08-20 23:06:48 -0500

    adding config file for config data

--- event.microservice/event.microservice/twitter-to-kafka-service/src/main/java/com/event/microservices/twitter/to/kafka/service/TwitterToKafkaServiceApplication.java
+++ event.microservice/event.microservice/twitter-to-kafka-service/src/main/java/com/event/microservices/twitter/to/kafka/service/TwitterToKafkaServiceApplication.java
@@ -1,25 +1,39 @@
 package com.event.microservices.twitter.to.kafka.service;
 
+import com.event.microservices.twitter.to.kafka.service.config.TwitterToKafkaServiceConfigData;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.ComponentScan;
 
 import javax.annotation.PostConstruct;
+import java.util.Arrays;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.event.microservices")
 public class TwitterToKafkaServiceApplication implements CommandLineRunner {
 
-    public static void main(String[] args){
+    public static final Logger LOG = LoggerFactory.getLogger(TwitterToKafkaServiceApplication.class);
+
+   public static void main(String[] args){
         SpringApplication.run(TwitterToKafkaServiceApplication.class,args);
     }
 
+    private TwitterToKafkaServiceConfigData twitterToKafkaServiceConfigData;
+
+    public TwitterToKafkaServiceApplication(TwitterToKafkaServiceConfigData configData){
+        this.twitterToKafkaServiceConfigData = configData;
+    }
 
     @Override
     public void run(String... args) throws Exception {
 
-        System.out.println("Application is running");
+        LOG.info("Application is running");
+        LOG.info(Arrays.toString( twitterToKafkaServiceConfigData.getTwitterKeywords().toArray(new String [] {})));
 
     }
 }
